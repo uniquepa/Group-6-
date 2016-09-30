0bb0acc964393caec389e6b6818ed65693454c24
Suji Hwang <suji.hwang16@imperial.ac.uk>
2016-09-30 18:24:54 +0100

    suji

--- kptest.R
+++ kptest.R
@@ -6,11 +6,8 @@ change second
 
 Hi Kim, I(Suji) want to add this sentence to your rmd.
 
-<<<<<<< HEAD
-
 
 creating some codes at line 14. hope this goes through!
-=======
+
 Suji second edit 
 3rd edit 
->>>>>>> 0a52443a528d3aeafecb532e8e3ed988ff6ab3cd
